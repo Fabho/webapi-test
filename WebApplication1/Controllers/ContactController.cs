46496613fe12f178330dad772fd846cba5320576
Fabho <fabian.calsina@gmail.com>
2017-11-29 18:16:32 -0400

    learning rest

---
+++ WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact() { Id=0, FirstName = "Fabian", LastName = "Calsina"}, 
+            new Contact() { Id=1, FirstName = "Fernando", LastName = "De la via"}, 
+            new Contact() { Id=2, FirstName = "Capitanazo", LastName = "Pacheco"} 
+        };
+        // GET: api/Contact
+        public IEnumerable<Contact> Get()
+        { 
+            return contacts;
+        }
+
+        // GET: api/Contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // POST: api/Contact
+        public IEnumerable<Contact> Post([FromBody] Contact newContact)
+        {
+            List<Contact> contactList = contacts.ToList();
+            newContact.Id = contactList.Count;
+
+            contactList.Add(newContact);
+            contacts = contactList.ToArray();
+
+            return contacts;
+        }
+
+        // PUT: api/Contact/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Contact/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
